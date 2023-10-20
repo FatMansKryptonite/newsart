8c62de4882622ffe3f2aa7fa5177f80e595bf3d4
Ivar Cashin Eriksson <ivar.cashin.eriksson@valcon.com>
2023-10-20 17:24:35 +0200

    GPT Api test running

---
+++ gpt_api_test.py
@@ -0,0 +1,22 @@
+
+import openai
+
+with open('api_keys/gpt_apikey.txt') as f:
+    openai.api_key = f.read()
+
+
+messages = [
+    {"role": "system", "content": "You are a intelligent assistant."}
+]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-4", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})

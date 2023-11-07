9c0d502cb3002599d50afcfbdbe665727536b42e
Ivar Cashin Eriksson <ivar.cashin.eriksson@gmail.com>
2023-11-07 17:46:21 +0100

    Starting article visualisation object development for tracking of generated results

---
+++ article_visualisation.py
@@ -0,0 +1,13 @@
+
+
+
+class article_visualisation:
+
+    def __init__(self):
+        self.article
+        self.image
+        self.date
+        self.dalle_prompt
+        self.modified_dalle_prompt
+        self.settings
+        self.article_keywords

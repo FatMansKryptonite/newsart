9ec95c90b59fd963ee1f7532cca4b3c601ceae69
Ivar Cashin Eriksson <ivar.cashin.eriksson@valcon.com>
2023-11-14 15:54:25 +0100

    Naive probability weighted style selector

--- style_selector.py
+++ style_selector.py
@@ -1,12 +1,14 @@
 import json
-from random import sample
+from numpy.random import choice
+import numpy as np
 
 
 with open('utils/art_styles.json', encoding='utf-8') as f:
     style_dict = json.load(f)
 
 
-def get_style():
-    style = sample(list(style_dict.keys()), 1)[0]
+def get_style(n: int = 1):
+    probabilities = np.array(list(style_dict.values())) / sum(style_dict.values())
+    chosen_styles = choice(list(style_dict.keys()), n, p=probabilities)
 
-    return style
+    return chosen_styles[0]
